778fd6d8c70f2818062452128bc51ec7da05b8b6
boham97 <kangbosung97@gmail.com>
2023-11-29 15:56:41 +0900

    dc 모터2개 제어 로직

--- motorLogic/motorLogic.ino
+++ motorLogic/motorLogic.ino
@@ -1,34 +1,54 @@
-long startTurn = 10L;
+long startTurn = 3L;
 long turn = 0L;
 long go = 0L;
 long right = 0L;
 long left = 0L;
 long up = 0L;
-String order = "SSSSSSSRULSSSSS";
+String order = "SRULSRUL";
 String status = "";
 int l, r;
 void setup(){
   Serial.begin(9600);
-  pinMode(12, OUTPUT);
+  pinMode(12, OUTPUT);        //12, 13 오른쪽 d6,d7
   pinMode(13, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(14, OUTPUT);        //14, 15 왼쪽 d5,d10
+  pinMode(15, OUTPUT);
+  pinMode(4, OUTPUT);         //서브모터 제어용
   digitalWrite(12, LOW);
   digitalWrite(13, LOW);
-}
-
-void unGo(){
-  digitalWrite(12, LOW);
-  digitalWrite(13, LOW);
-}
-void unRight(){
-  digitalWrite(12, LOW);
-}
-void unLeft(){
-  digitalWrite(13, LOW);
-}
-void unUp(){
+  digitalWrite(14, LOW);
+  digitalWrite(15, LOW);
   digitalWrite(4, LOW);
 }
+
+void goStraight(){
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  digitalWrite(14, HIGH);
+  digitalWrite(15, LOW);
+}
+
+void goRight(){
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  digitalWrite(14, LOW);
+  digitalWrite(15, HIGH);
+}
+
+void goLeft(){
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  digitalWrite(14, HIGH);
+  digitalWrite(15, LOW);
+}
+
+void stop(){
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  digitalWrite(14, LOW);
+  digitalWrite(15, LOW);
+}
+
 
 
 void readOrder(){
@@ -39,18 +59,15 @@ void readOrder(){
     switch(now){
       case 'S':
         go = turn + 1;
-        digitalWrite(12, HIGH);
-        digitalWrite(13, HIGH);
-        Serial.print(turn);
-        Serial.println("go!");
+        goStraight();
         break;
       case 'R':
         right = turn + 1;
-        digitalWrite(12, HIGH);
+        goRight();
         break;
       case 'L':
         left = turn + 1;
-        digitalWrite(13, HIGH);
+        goLeft();
         break;
       case 'U':
         up = turn + 1;
@@ -72,14 +89,20 @@ void loop(){
   }
 
   if(go <= turn && right <= turn){
-    unRight();
+    digitalWrite(12, LOW);
   }
 
-  if(go <= turn && left <= turn){
-    unLeft();
+  if(left <= turn){
+    digitalWrite(13, LOW);
+  }
+    if(go <= turn && left <= turn){
+    digitalWrite(14, LOW);
+  }
+  if(right <= turn){
+    digitalWrite(15, LOW);
   }
   if(up <= turn){
-    unUp();
+    digitalWrite(4, LOW);
   }
 
 }
